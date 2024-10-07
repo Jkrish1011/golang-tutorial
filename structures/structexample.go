63bcfd01991552a709febf062774f656fa70ee97
Jayakrishnan Ashok <jayakrishnanashok@gmail.com>
2024-10-07 15:10:30 +0530

    nested-structures

--- structures/structexample.go
+++ structures/structexample.go
@@ -1,14 +1,30 @@
 package structures
 
 type car struct {
-	model string
-	make  int
+	model      string
+	make       int
+	frontWheel wheel
+	backWheel  wheel
+}
+
+type wheel struct {
+	radius   int
+	material string
 }
 
 func carStruct() car {
+
 	car1 := car{
 		model: "VW",
 		make:  2023,
+		frontWheel: wheel{
+			radius:   10,
+			material: "alloy",
+		},
+		backWheel: wheel{
+			radius:   12,
+			material: "steel",
+		},
 	}
 	return car1
 }
--- structures/structexample_test.go
+++ structures/structexample_test.go
@@ -5,4 +5,5 @@ import "testing"
 func TestCarStruct(t *testing.T) {
 	car := carStruct()
 	t.Logf("car is %v and make is %d", car.model, car.make)
+	t.Logf("frontWheel is %+v and backWheel is %+v", car.frontWheel, car.backWheel)
 }
