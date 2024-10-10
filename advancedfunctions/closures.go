008cfdd94b049ca47248bebd494ce76af66d7bf1
Jayakrishnan Ashok <jayakrishnanashok@gmail.com>
2024-10-10 22:21:50 +0530

    closure-documentation-update

--- advancedfunctions/closures.go
+++ advancedfunctions/closures.go
@@ -5,6 +5,14 @@ import "fmt"
 /*
 Closures are functions which reference variables outside of it's own function body.
 The function may access and assign to the referenced variables
+
+Note:
+They just return a function and the logic inside that function is what is important.
+the signature of the returned function, what you do with it, and what are the other variables you declare outside the
+scope of the return function everything counts.! It's important to think through the requirement before implementing closures.
+
+NOTE:
+The function that we are returning has no name, making them anonymous.
 */
 
 // This function will keep concatinating the strings on which it is being passed into
